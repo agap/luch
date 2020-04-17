8ffcf4fb91a1d134829bb6eabab63af8e4445896
Artem Gapchenko <artemgapchenko@yandex.ru>
2020-04-17 18:01:34 +0400

    Move BeaconScanCallback into BeaconScanner.

--- ibeacon/src/main/java/aga/android/ibeacon/BeaconScanner.java
+++ ibeacon/src/main/java/aga/android/ibeacon/BeaconScanner.java
@@ -1,16 +1,29 @@
 package aga.android.ibeacon;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.os.Handler;
+import android.util.Log;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static android.os.SystemClock.elapsedRealtime;
 
 // todo make package private
 public class BeaconScanner implements IScanner {
+
+    private static final String TAG = "BeaconScanner";
 
     private static final ScanSettings SCAN_SETTINGS = new ScanSettings.Builder()
         .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
@@ -21,6 +34,46 @@ public class BeaconScanner implements IScanner {
 
     @NonNull
     private final BeaconScanCallback scanCallback = new BeaconScanCallback();
+
+    // todo make it possible to change this value
+    private final long beaconEvictionTimeMillis = TimeUnit.SECONDS.toMillis(5);
+
+    private final Runnable beaconEvictionTask = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "Running the beacon eviction task");
+
+            final Iterator<Beacon> iterator = nearbyBeacons.keySet().iterator();
+
+            for (; iterator.hasNext(); ) {
+                final Beacon inMemoryBeacon = iterator.next();
+                final Long lastAppearanceMillis = nearbyBeacons.get(inMemoryBeacon);
+
+                Log.d(TAG, "Elapsed real time is: " + elapsedRealtime() +
+                        "; last appearance time is: " + lastAppearanceMillis +
+                        "; eviction period is: " + beaconEvictionTimeMillis);
+
+                if (lastAppearanceMillis != null
+                        && elapsedRealtime() - lastAppearanceMillis > beaconEvictionTimeMillis) {
+
+                    iterator.remove();
+                }
+            }
+
+            if (beaconListener != null) {
+                beaconListener.onNearbyBeaconsDetected(nearbyBeacons.keySet());
+            }
+
+            handler.postDelayed(this, beaconEvictionTimeMillis);
+        }
+    };
+
+    @Nullable
+    private IBeaconListener beaconListener = null;
+
+    private final Handler handler = new Handler();
+
+    private final Map<Beacon, Long> nearbyBeacons = new HashMap<>();
 
     @NonNull
     private List<ScanFilter> scanFilters = Collections.emptyList();
@@ -34,8 +87,8 @@ public class BeaconScanner implements IScanner {
     }
 
     @Override
-    public void setBeaconListener(IBeaconListener listener) {
-        scanCallback.setListener(listener);
+    public void setBeaconListener(@NonNull IBeaconListener listener) {
+        beaconListener = listener;
     }
 
     @Override
@@ -52,6 +105,8 @@ public class BeaconScanner implements IScanner {
                 SCAN_SETTINGS,
                 scanCallback
             );
+
+        handler.postDelayed(beaconEvictionTask, beaconEvictionTimeMillis);
     }
 
     @Override
@@ -60,8 +115,34 @@ public class BeaconScanner implements IScanner {
             .getBluetoothLeScanner()
             .stopScan(scanCallback);
 
-        //todo I don't like it at all; looks like BeaconScanner and BeaconScanCallback
-        // _should be_ merged into one
-        scanCallback.stop();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    private final class BeaconScanCallback extends ScanCallback {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            final Beacon beacon = RegionDefinitionMapper.asBeacon(result);
+
+            if (beacon == null) {
+                return;
+            }
+
+            nearbyBeacons.put(beacon, elapsedRealtime());
+
+            if (beaconListener != null) {
+                beaconListener.onNearbyBeaconsDetected(nearbyBeacons.keySet());
+            }
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            Log.d(TAG, "On batch scan results: " + results);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.d(TAG, "On scan failed: " + errorCode);
+        }
     }
 }
