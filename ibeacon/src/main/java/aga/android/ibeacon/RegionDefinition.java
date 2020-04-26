b55b3c0b8aca4e746b3bb34c9ad2ef607d0a345b
Artem Gapchenko <artemgapchenko@yandex.ru>
2020-04-26 17:19:34 +0400

    Raise errors from RegionDefinition's constructor to enforce expectations.

--- ibeacon/src/main/java/aga/android/ibeacon/RegionDefinition.java
+++ ibeacon/src/main/java/aga/android/ibeacon/RegionDefinition.java
@@ -1,5 +1,6 @@
 package aga.android.ibeacon;
 
+import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -14,7 +15,26 @@ public class RegionDefinition {
     @Nullable
     private final Integer minor;
 
-    public RegionDefinition(@NonNull String uuid, @Nullable Integer major, @Nullable Integer minor) {
+    public RegionDefinition(@NonNull String uuid,
+                            @Nullable @IntRange(from = 0, to = 65535) Integer major,
+                            @Nullable @IntRange(from = 0, to = 65535) Integer minor) {
+
+        if (major != null && (major < 0 || major > 65535)) {
+            throw new AssertionError("Major has to be in range [0, 65535]; " +
+                    "actual value is: " + major);
+        }
+
+        if (minor != null && (minor < 0 || minor > 65535)) {
+            throw new AssertionError("Minor has to be in range [0, 65535]; " +
+                    "actual value is: " + minor);
+        }
+
+        if ((major != null && minor == null) || (major == null && minor != null)) {
+            throw new AssertionError("Both major and minor are expected to be either " +
+                    "non-null or nullable at the same time; actual major is: " + major +
+                    ", actual minor is: " + minor);
+        }
+
         this.uuid = uuid;
         this.major = major;
         this.minor = minor;
