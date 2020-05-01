4f88346ef298c7544384d8d1dde24b7a64cc2478
Artem Gapchenko <artemgapchenko@yandex.ru>
2020-05-01 13:36:53 +0400

    Add one more test to validate the BeaconScanner's behaviour.
    The test validates that the BeaconScanner correctly parses the ScanResult it obtains via a ScanCallback and retransmits the resulting Beacon into the IBeaconListener.

--- luch/src/test/java/aga/android/luch/BeaconScannerTest.java
+++ luch/src/test/java/aga/android/luch/BeaconScannerTest.java
@@ -1,15 +1,24 @@
 package aga.android.luch;
 
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import androidx.annotation.NonNull;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.junit.Assert.*;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static aga.android.luch.TestHelpers.createScanResult;
+import static java.util.Collections.singleton;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 21, manifest = Config.NONE)
@@ -18,7 +27,7 @@ public class BeaconScannerTest {
     @Test
     public void testScannerStartsAndStopsScansViaBleDevice() {
         // given
-        final FakeBleDevice bleDevice = new FakeBleDevice();
+        final CountingFakeBleDevice bleDevice = new CountingFakeBleDevice();
 
         final BeaconScanner scanner = new BeaconScanner
             .Builder()
@@ -36,7 +45,93 @@ public class BeaconScannerTest {
         assertEquals(2, bleDevice.stops);
     }
 
-    private static final class FakeBleDevice implements IBleDevice {
+    @Test
+    public void testScannerRetransmitsBeaconsToIBeaconListener()
+            throws InvocationTargetException,
+                    NoSuchMethodException,
+                    InstantiationException,
+                    IllegalAccessException {
+        // given
+        final RetransmittingFakeBleDevice bleDevice = new RetransmittingFakeBleDevice();
+
+        final RecordingBeaconListener beaconListener = new RecordingBeaconListener();
+
+        final BeaconScanner scanner = new BeaconScanner
+            .Builder()
+            .setBleDevice(bleDevice)
+            .setBeaconListener(beaconListener)
+            .build();
+
+        final String bluetoothAddress = "00:11:22:33:FF:EE";
+        final String proximityUuid = "E56E1F2C-C756-476F-8323-8D1F9CD245EA";
+        final int rssi = -95;
+        final int major = 15600;
+        final int minor = 395;
+        final ScanResult scanResult = createScanResult(
+            bluetoothAddress,
+            proximityUuid,
+            major,
+            minor,
+            rssi
+        );
+
+        // when
+        scanner.start();
+        bleDevice.transmitScanResult(scanResult);
+        scanner.stop();
+
+        // then
+        assertEquals(
+            singleton(
+                new Beacon(proximityUuid, bluetoothAddress, major, minor, rssi)
+            ),
+            beaconListener.nearbyBeacons
+        );
+    }
+
+    /**
+     * In real life the {@link SystemBleDevice} instance will pass the {@link ScanCallback} instance
+     * into the {@link android.bluetooth.le.BluetoothLeScanner} which will later call it with the
+     * {@link ScanResult} as an argument. Since we replace the {@link SystemBleDevice} with
+     * {@link RetransmittingFakeBleDevice} in tests, we'll just expose the method that allows us to
+     * send an instance of {@link ScanResult} into the {@link ScanCallback} provided by
+     * {@link BeaconScanner} and validate that the ScanResult -> Beacon conversion works smooth
+     * and {@link IBeaconListener} instance is called.
+     */
+    private static final class RetransmittingFakeBleDevice implements IBleDevice {
+
+        @Nullable
+        private ScanCallback scanCallback;
+
+        void transmitScanResult(@NonNull ScanResult scanResult) {
+            if (scanCallback != null) {
+                scanCallback.onScanResult(ScanSettings.CALLBACK_TYPE_ALL_MATCHES, scanResult);
+            }
+        }
+
+        @Override
+        public void startScans(@NonNull ScanCallback scanCallback) {
+            this.scanCallback = scanCallback;
+        }
+
+        @Override
+        public void stopScans(@NonNull ScanCallback scanCallback) {
+            this.scanCallback = scanCallback;
+        }
+    }
+
+    private static final class RecordingBeaconListener implements IBeaconListener {
+
+        final Set<Beacon> nearbyBeacons = new HashSet<>();
+
+        @Override
+        public void onNearbyBeaconsDetected(@NonNull Set<Beacon> beacons) {
+            nearbyBeacons.clear();
+            nearbyBeacons.addAll(beacons);
+        }
+    }
+
+    private static final class CountingFakeBleDevice implements IBleDevice {
 
         private int starts = 0;
         private int stops = 0;
