39ce09ddf745ffe57a771cc0f5e86aa4c2ba96e7
Artem Gapchenko <artemgapchenko@yandex.ru>
2020-05-03 22:34:08 +0400

    Add SystemBleDevice tests.

---
+++ luch/src/test/java/aga/android/luch/SystemBleDeviceTest.java
@@ -0,0 +1,116 @@
+package aga.android.luch;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 21, manifest = Config.NONE)
+public class SystemBleDeviceTest {
+
+    private final BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+
+    private final BluetoothLeScanner scanner = mock(BluetoothLeScanner.class);
+
+    private final ScanSettings scanSettings = new ScanSettings.Builder().build();
+
+    private List<ScanFilter> scanFilters = Collections.emptyList();
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+        }
+    };
+
+
+    @Test
+    public void testScansWillBeStartedIfThereIsBleScannerInBluetoothAdapter() {
+
+        // given
+        final SystemBleDevice device = new SystemBleDevice(
+            adapter,
+            scanSettings,
+            scanFilters
+        );
+        when(adapter.getBluetoothLeScanner()).thenReturn(scanner);
+
+        // when
+        device.startScans(scanCallback);
+
+        // then
+        verify(scanner).startScan(scanFilters, scanSettings, scanCallback);
+        verifyNoMoreInteractions(scanner);
+    }
+
+    @Test
+    public void testScansWillBeStoppedIfThereIsBleScannerInBluetoothAdapter() {
+
+        // given
+        final SystemBleDevice device = new SystemBleDevice(
+            adapter,
+            scanSettings,
+            scanFilters
+        );
+        when(adapter.getBluetoothLeScanner()).thenReturn(scanner);
+
+        // when
+        device.stopScans(scanCallback);
+
+        // then
+        verify(scanner).stopScan(scanCallback);
+        verifyNoMoreInteractions(scanner);
+    }
+
+    @Test
+    public void testNoExceptionIsThrownWhileTryingToStartScansWhenThereIsNoBleScanner() {
+        // given
+        final SystemBleDevice device = new SystemBleDevice(
+            adapter,
+            scanSettings,
+            scanFilters
+        );
+        when(adapter.getBluetoothLeScanner()).thenReturn(null);
+
+        // when
+        device.startScans(scanCallback);
+
+        // then
+        verify(adapter).getBluetoothLeScanner();
+        verifyNoMoreInteractions(adapter);
+    }
+
+    @Test
+    public void testNoExceptionIsThrownWhileTryingToStopScansWhenThereIsNoBleScanner() {
+        // given
+        final SystemBleDevice device = new SystemBleDevice(
+            adapter,
+            scanSettings,
+            scanFilters
+        );
+        when(adapter.getBluetoothLeScanner()).thenReturn(null);
+
+        // when
+        device.stopScans(scanCallback);
+
+        // then
+        verify(adapter).getBluetoothLeScanner();
+        verifyNoMoreInteractions(adapter);
+    }
+}
