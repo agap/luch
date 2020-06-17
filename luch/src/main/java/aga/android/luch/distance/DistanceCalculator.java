f2def081dc8b122b985ed35a91fe455426aba4e1
Artem Gapchenko <artemgapchenko@yandex.ru>
2020-06-17 23:54:34 +0400

    Remove forgotten todo.

--- luch/src/main/java/aga/android/luch/distance/DistanceCalculator.java
+++ luch/src/main/java/aga/android/luch/distance/DistanceCalculator.java
@@ -9,7 +9,6 @@ import static java.lang.Math.pow;
  * Simple distance calculator based on the paper called "Evaluation of the Reliability of RSSI
  * for Indoor Localization". See here: https://www.rn.inf.tu-dresden.de/dargie/papers/icwcuca.pdf
  */
-//todo test me
 class DistanceCalculator extends AbstractDistanceCalculator {
 
     protected DistanceCalculator(int txPowerPosition) {
